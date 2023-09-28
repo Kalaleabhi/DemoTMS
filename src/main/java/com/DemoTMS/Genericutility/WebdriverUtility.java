5e23b3f1a3278da4f8cbf0487966ab814e876ab0
admin <admin@DESKTOP-1C8JGEG>
2023-09-28 11:15:57 +0530

    first commit

---
+++ src/main/java/com/DemoTMS/Genericutility/WebdriverUtility.java
@@ -0,0 +1,12 @@
+package com.DemoTMS.Genericutility;
+
+import org.testng.annotations.Test;
+
+public class WebdriverUtility {
+
+	@Test
+	public void maximize()
+	{
+		System.out.println("--from maximmize browser--");
+	}
+}
