f8d87e758f4f5403397e71e90328227c3c63f4ae
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-09-28 11:59:52 +0530

    Admin 1 st commit

---
+++ src/test/java/createtourpackagemodel/CreateTourPackTest.java
@@ -0,0 +1,12 @@
+package createtourpackagemodel;
+
+import org.testng.annotations.Test;
+
+public class CreateTourPackTest {
+
+	@Test
+	public void cretetourPackTest()
+	{
+		System.out.println("--From creat tour pack test");
+	}
+}
