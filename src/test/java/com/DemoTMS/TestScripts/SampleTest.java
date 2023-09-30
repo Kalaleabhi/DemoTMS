c3d746c89e865d7381ef8032a93d41be5f0eadf4
kalaleabhi <abhishekkalale19@gmail.com>
2023-09-30 15:51:34 +0530

    2nd commit

---
+++ src/test/java/com/DemoTMS/TestScripts/SampleTest.java
@@ -0,0 +1,12 @@
+package com.DemoTMS.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("--mysore");
+	}
+}
