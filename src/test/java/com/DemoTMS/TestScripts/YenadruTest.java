f6e52c0786402352e6c75890027c42bfefa227c4
kalaleabhi <abhishekkalale19@gmail.com>
2023-09-30 13:34:54 +0530

    final commit

---
+++ src/test/java/com/DemoTMS/TestScripts/YenadruTest.java
@@ -0,0 +1,12 @@
+package com.DemoTMS.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class YenadruTest {
+
+	@Test
+	public void YenadruTest()
+	{
+		System.out.println("--Yake--");
+	}
+}
